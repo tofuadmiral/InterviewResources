5774572fc3de6e008eafb05fa5aed23762acad1f
Ahmed Fuad Ali <fuad98ali@gmail.com>
2019-07-25 14:29:31 -0400

    first version of coding assignment

--- fall2019/elevator.py
+++ fall2019/elevator.py
@@ -12,7 +12,7 @@ The following class models an elevator. This includes processing an array of ins
 displaying it's final position and actually peforming the required carloads. 
 
 
-some notes:
+some Ambiguities:
 
 always moves up first and then down:
     interpreted that to mean given a set of instructions with both floors going up and down
@@ -20,7 +20,18 @@ always moves up first and then down:
     then we'll move to the one up first and then the one down
     (in this example, move to five first and then move to three)
 
-won't move unless someone pushes a button
+No total floor count given for the building:
+    because this wasn't specified, I assumed a building of 30 floors, 
+    and in each of my methods I added the # of floors serviced to 
+    be a constraint of the problem. this can be customized by the user
+
+Assumptions
+    I didn't take into account certain edge cases, given the practical nature of the problem.
+    Some examples: I didn't account for negative values in my arrays, since I assummed 
+    that there would only be floors above ground. This can be implemented pretty easily. 
+
+    when letting people out of the elevator, no one comes in at that floor. otherwise, the 
+    set of input carloads would change, and we would have to account for that. 
 
 
 
@@ -29,18 +40,62 @@ won't move unless someone pushes a button
 
 class Elevator:
 
-    # class attributes
-
-
     # initializer
-    def __init__(self, floors):
+    def __init__(self, floors, start):
         self.floors = floors
+        self.start = start
 
-    # instance methods 
-    pass
+    # instance methods
+    def process_floors(self, arr):
+        # takes in an array of arrays of carloads, outputs 
+        # how many stops, carloads processed, floors it passed, final floor
+        
+        # initialize outputs
+        total_carloads = len(arr)
+        total_stops = 0
+        floors_passed = 0
+
+        # keep track of what floor we're at 
+        position = self.start 
+
+        # remove duplicates in carload and sort them from least to greatest 
+        arr = list(map(lambda x: sorted(list(set(x))), arr))
+
+        print("Array after sorting and removing duplicates: ", arr)
+
+        # now, for each carload, determine movements
+        for i in arr:
+            floors_below = []
+            total_stops += len(i)
+
+            # traverse floors above as per rule 1 (travels up first)
+            for j in i:
+                if j > position:
+                    floors_passed += abs(j - position)
+                    position = j
+                else:
+                    floors_below.append(j)
+            
+            # then traverse floors below, from 
+            for k in reversed(floors_below): # reverse this to be in order from highest bottom floor to least
+                floors_passed += abs(k - position)
+                position = k
+
+        # Ouput results
+        print("Total carlods:", total_carloads)
+        print("  Total stops:", total_stops)
+        print("Floors passed:", floors_passed)
+        print("  Final floor:", position)
 
 
-def if __name__ == "__main__":
+# MAIN FUNCTION
+
+if __name__ == "__main__":
     # let's define some test cases
+    test1 = [[3, 1, 4], [2, 8, 4], [4, 6, 4, 9]]
 
-    pass
+
+    # change these values to 
+    elevator1 = Elevator(30, 5)
+
+    elevator1.process_floors(test1)
